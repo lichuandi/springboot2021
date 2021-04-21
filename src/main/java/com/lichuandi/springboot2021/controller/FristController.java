728e505b40928f66643e16a8e4251fab86e87c12
lichuandi <your_email@mail.com>
2021-04-22 00:27:56 +0800

    修改yml文件

--- src/main/java/com/lichuandi/springboot2021/controller/FristController.java
+++ src/main/java/com/lichuandi/springboot2021/controller/FristController.java
@@ -1,5 +1,8 @@
 package com.lichuandi.springboot2021.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description：com.lichuandi.springboot2021.controller version：1
  */
 @RestController
+//@Configuration
+//@ConfigurationProperties(prefix = "my")
 public class FristController {
     @GetMapping("/hi")
     public String Hi(){
         return "lichuandi";
+    }
+    @Value("${my.name}")
+    private String name;
+    @Value("${my.age}")
+    private int age;
+    @RequestMapping(value = "/myname")
+    public  String myName(){
+        return "myname is "+name+"and"+age;
     }
 }
