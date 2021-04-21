8f58f40338f178139968870914bc22470172bad8
lichuandi <your_email@mail.com>
2021-04-21 23:49:38 +0800

    注解和测试的代码提交

--- src/test/java/com/lichuandi/springboot2021/Springboot2021ApplicationTests.java
+++ src/test/java/com/lichuandi/springboot2021/Springboot2021ApplicationTests.java
@@ -1,13 +1,43 @@
 package com.lichuandi.springboot2021;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+import java.net.URL;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class Springboot2021ApplicationTests {
 
+    @LocalServerPort
+    private int port;
+    private URL base;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Before
+    public void setUp() throws Exception{
+        this.base =new URL("http://localhost:"+port+"/hello");
+    }
+    @Test
+    public void getHello() throws Exception{
+        ResponseEntity<String> response=testRestTemplate.getForEntity(base.toString(),String.class);
+       // assertThat(response.getBody()),equalTo("Greeting from Spring Boot"));
+    }
     @Test
     void contextLoads() {
+
     }
 
 }
