acf3bf09e4029a827b85b5db38a2edc5f0ad2d6a
lichuandi <your_email@mail.com>
2021-04-21 20:11:04 +0800

    提交同步

--- src/main/java/com/lichuandi/springboot2021/Springboot2021Application.java
+++ src/main/java/com/lichuandi/springboot2021/Springboot2021Application.java
@@ -2,8 +2,9 @@ package com.lichuandi.springboot2021;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class Springboot2021Application {
 
     public static void main(String[] args) {
