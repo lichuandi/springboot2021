644044fb6aaff7177134a1b1559f9058f978bf4c
lichuandi <your_email@mail.com>
2021-04-21 18:31:06 +0800

    第一次提交springboot2021代码

---
+++ src/main/java/com/lichuandi/springboot2021/Springboot2021Application.java
@@ -0,0 +1,13 @@
+package com.lichuandi.springboot2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2021Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot2021Application.class, args);
+    }
+
+}
